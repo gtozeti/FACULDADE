07ef40501a6fb10b0ade16f51e0e47331ff692c3
gtozeti <78425766+gtozeti@users.noreply.github.com>
2021-02-28 16:22:48 -0300

    Add files via upload

---
+++ 1º - Semestre/PI 1_ 2101_PROJETO INTEGRADOR DESENVOLVIMENTO DE LÓGICA/ADO_02/EX_2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class EX_2 {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Digite um número inteiro: ");
+		int N = input.nextInt();
+		
+		System.out.println("O primeiro múltiplo de "+ N +" é: "+ N +"\nO segundo múltiplo de " + N +" é: "+ N*2+"\nO terceiro múltiplo de " + N +" é: "+ N*3);
+
+	}
+
+}
