eccdfd3d7e186f7779b837959d84cb23928fdd88
gtozeti <78425766+gtozeti@users.noreply.github.com>
2021-05-24 15:48:39 -0300

    Create Lista02_Ex1.java

---
+++ 1º - Semestre/AP 1_2101_ALGORITMOS E PROGRAMAÇÃO I/Lista_02/Lista02_Ex1.java
@@ -0,0 +1,13 @@
+//Faça um programa que mostre todos os números inteiros de 1 a 50. Use o laço WHILE.
+public class Lista02_Ex1 {
+
+	public static void main(String[] args) {
+		int n = 1;
+		while (n<51) {
+			System.out.println(n);
+			n++;
+		}
+
+	}
+
+}
