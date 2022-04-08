56cc701f574e9c415b36b92504aca9187955ba83
gtozeti <gtozeti@gmail.com>
2022-04-07 22:45:47 -0300

    Exercícios da ADO3

---
+++ 3º - Semestre/POO_PROGRAMAÇÃO ORIENTADA A OBJETOS/EX3/POO_A_A03_GustavoTozetiHerculano/Beneficiario.java
@@ -0,0 +1,41 @@
+// Superclasse abstrata
+public abstract class Beneficiario {
+
+// Atributos da superclasse    
+private String nome;
+private String cpf;
+private int idade;
+
+// Contrutor da superclasse   
+public Beneficiario( String nome, String cpf, int idade){
+    this.nome = nome;
+    this.cpf = cpf;
+    this.idade = idade;
+}
+
+
+public String getNome() {
+    return nome;
+}
+
+public String getCpf() {
+    return cpf;
+}
+
+public int getIdade() {
+    return idade;
+}
+
+// Método abstrato
+public abstract String autorizaConsulta();
+
+
+// Override do método toString
+@Override
+public String toString() {
+    return "O(a) paciente " + getNome() + ", com CPF "+ getCpf() + " e idade " + getIdade();
+}
+
+
+
+}
---
+++ 3º - Semestre/POO_PROGRAMAÇÃO ORIENTADA A OBJETOS/EX3/POO_A_A03_GustavoTozetiHerculano/PlanoA.java
@@ -0,0 +1,54 @@
+public class PlanoA  extends Beneficiario{
+    
+    // Atributos da subclasse
+    private String exame1;
+    private String exame2;
+
+    // Contrutor da subclasse
+    public PlanoA(String nome, String cpf, int idade, String exame1, String exame2){
+        super(nome, cpf, idade);
+        this.exame1 = exame1;
+        this.exame2 = exame2;
+    }
+
+    public String getExame1() {
+        return exame1;
+    }
+
+    public void setExame1(String exame1) {
+        this.exame1 = exame1;
+    }
+
+    public String getExame2() {
+        return exame2;
+    }
+
+    public void setExame2(String exame2) {
+        this.exame2 = exame2;
+    }
+
+    // Override do método abstrato autorizaConsulta da superclasse, para retorno de String de status de autorização do exame
+    @Override
+    public String autorizaConsulta(){
+        if (exame1 == "Oftalmologista" && getIdade() < 18){
+            return "NÃO AUTORIZADO";
+        }
+        
+        else if(exame2 == "Dermatologista" && getIdade() == 45)
+            return "NÃO AUTORIZADO";
+        
+        else{
+            return "AUTORIZADO";
+        }
+        }
+
+    // Override do método toString da superclasse, para retorno do método toString da superclasse mais as informações de exames e autorização
+    @Override
+    public String toString(){
+        return String.format("\n%s, está marcando os exames %s e %s. Status do pedido: %s.",super.toString(), getExame1(), getExame2(), autorizaConsulta());
+    }
+        
+    }
+    
+
+
