47ea9b697e1b6dbc5691d957672e2ddf77c69efe
gtozeti <gtozeti@gmail.com>
2022-04-16 14:53:08 -0300

    Att

---
+++ 3º - Semestre/ED_ESTRUTURA DE DADOS/P1/ListaSimples.java
@@ -0,0 +1,135 @@
+public class ListaSimples {
+
+    private Elemento inicio = null, atual, aux;
+
+    public void inserir(Object objeto) {
+        if (inicio == null) {
+
+            inicio = new Elemento(objeto, null);
+            aux = inicio;
+
+        } else {
+
+            atual = new Elemento(objeto, null);
+            aux.setProx(atual);
+            aux = atual;
+
+        }
+    }
+
+    public void exibir() {
+        Elemento x = inicio;
+        while (x != null) {
+            System.out.println(x.getObjeto());
+            x = x.getProx();
+        }
+    }
+
+    public Object retornaProduto(int id) {
+        Elemento x = inicio;
+
+        while (x != null) {
+            Produto y = (Produto) x.getObjeto();
+            if (y.getId() == id) {
+                return y;
+
+            }
+            x = x.getProx();
+        }
+        return String.format("\nO Produto com o ID %d não existe", id);
+        
+    }
+
+    public double valorTotal(){
+        Elemento x = inicio;
+        double soma = 0.0;
+
+        while (x != null) {
+            Produto y = (Produto) x.getObjeto();
+            soma += y.getPreco();
+            x = x.getProx();
+
+            }
+            return soma;
+        }
+    
+
+    public boolean remover(int id) {
+        Elemento[] v = pesquisarRemove(id);
+        if (v != null) { // Verifica se achou o elemento que quer remover
+            Elemento x = v[0];
+            Elemento auxRemove = v[1];
+            if (x == inicio) {
+                inicio = x.getProx();
+                x.setProx(null);
+                return true;
+            } 
+            else if (x == atual) {
+                atual = auxRemove;
+                aux = auxRemove;
+                auxRemove.setProx(null);
+                return true;
+            }
+            else {
+                auxRemove.setProx(x.getProx());
+                x.setProx(null);
+                return true;
+            }
+        } else {
+            return false;
+        }
+
+    }
+
+    private Elemento[] pesquisarRemove(int id) {
+        Elemento x = inicio, auxRem = null;
+        Elemento[] v = { x, auxRem };
+        Produto c;
+        while (x != null) {
+            c = (Produto) x.getObjeto();
+            if (id == c.getId()) {
+                v[0] = x;
+                v[1] = auxRem;
+                return v;
+            }
+            auxRem = x;
+            x = x.getProx();
+        }
+        return null;
+    }
+
+    // Inner Class
+    private class Elemento {
+
+        private Object objeto;
+        private Elemento prox;
+
+        public Elemento(Object objeto, Elemento prox) {
+            this.objeto = objeto;
+            this.prox = prox;
+        }
+
+        public Object getObjeto() {
+            return objeto;
+        }
+
+        public void setObjeto(Object objeto) {
+            this.objeto = objeto;
+        }
+
+        public Elemento getProx() {
+            return prox;
+        }
+
+        public void setProx(Elemento prox) {
+            this.prox = prox;
+        }
+
+        @Override
+        public String toString() {
+            return "Elemento [objeto=" + objeto + ", prox=" + prox + "]";
+        }
+
+    }
+
+}
