f74792664b6f207bf04799f1a0bbbdcbb5708559
gtozeti <gtozeti@gmail.com>
2022-11-05 21:03:51 -0300

    Massa PI

---
+++ 4º - Semestre/PI_PROJETO INTEGRADOR/Massas Cadastro/massas.py
@@ -0,0 +1,35 @@
+import requests
+from fordev.generators import people
+
+URL = 'http://192.168.15.16:8080/'
+
+login = {
+    "email": "manhattan@email.com",
+    "password": "senha123"
+}
+
+h = {
+    'Content-Type': 'application/json'
+}
+
+
+req = requests.post(f'{URL}profissional/login',json=login, headers=h)
+tk = req.headers['Authorization']
+
+
+dados = people(n=30)
+
+for d in dados:
+
+    p = {   "email": d['email'],
+            "nome" : d['nome'].split()[0],
+            "sobrenome" : d['nome'].replace(d['nome'].split()[0], "").strip(),
+            "cpf" : d['cpf'],
+            "dataNascimento" : f'{d["data_nasc"].split("/")[2]}-{d["data_nasc"].split("/")[1]}-{d["data_nasc"].split("/")[0]}',
+            "genero" : d['sexo'].upper()
+    }
+    requests.post(f'{URL}cliente/register',json=p, headers={
+        "Authorization": f'Bearer {tk}',
+        'Content-Type': 'application/json'
+    })
+
